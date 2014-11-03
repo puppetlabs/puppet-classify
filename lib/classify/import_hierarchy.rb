7dae8b3fa9c272d77be64bc22956e7022ef560bd
Brian Cain <brian.cain@puppetlabs.com>
2014-11-03 09:55:25 -0800

    Add error handling for import_hierarchy endpoint

--- lib/classify/import_hierarchy.rb
+++ lib/classify/import_hierarchy.rb
@@ -8,5 +8,10 @@ class ImportHierarchy
 
   def import(group_hierarchy)
     import_res = @puppet_https.post("#{@nc_api_url}/v1/import-hierarchy", group_hierarchy.to_json)
+
+    if import_res.code.to_i >= 400
+      puts "An error has occured during import: HTTP #{import_res.code} #{import_res.message}"
+      puts import_res.body
+    end
   end
 end
