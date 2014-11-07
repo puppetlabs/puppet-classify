062695ee87cce7b29fc21ce9b7784c0d48b69220
Brian Cain <brian.cain@puppetlabs.com>
2014-11-07 10:13:37 -0800

    Add environment and update_classes spec tests

---
+++ spec/classify/environments_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require_relative '../../lib/classify/environments.rb'
+require_relative '../../lib/classify'
+
+describe Environments do
+  before :each do
+    @classifier_url = 'https://puppetmaster.local:4433/classifier-api'
+
+    auth_info = {
+      "ca_certificate_path" => "/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem",
+      "certificate_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem",
+      "private_key_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem"
+    }
+
+    @classify = Classify.new(@classifier_url, auth_info)
+  end
+
+  describe "#get_environments" do
+    let(:response_body) { response_body = "[{\"name\":\"production\"}]" }
+
+    it "returns an Array of environments" do
+      stub_request(:get, "#{@classifier_url}/v1/environments").
+        with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(:status => 200, :body => response_body, :headers => {})
+      expect(@classify.environments.get_environments).to be_an_instance_of Array
+    end
+  end
+
+  describe "#get_environment" do
+    let(:response_body) { response_body = "{\"name\":\"production\"}" }
+    let(:name) { name = 'production' }
+
+    it "returns a Hash of the specified environment" do
+      stub_request(:get, "#{@classifier_url}/v1/environments/#{name}").
+        with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(:status => 200, :body => response_body, :headers => {})
+      expect(@classify.environments.get_environment(name)).to be_an_instance_of Hash
+    end
+  end
+
+  describe "#create_environment" do
+    let(:name) { name = 'development' }
+
+    it "creates an environment given a name" do
+      stub_request(:put, "#{@classifier_url}/v1/environments/#{name}").
+                 with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+                 to_return(:status => 201, :body => "", :headers => {})
+      expect(@classify.environments.create_environment(name)).to be_nil
+    end
+  end
+end
---
+++ spec/classify/update_classes_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require_relative '../../lib/classify/update_classes.rb'
+require_relative '../../lib/classify'
+
+describe UpdateClasses do
+  before :each do
+    @classifier_url = 'https://puppetmaster.local:4433/classifier-api'
+
+    auth_info = {
+      "ca_certificate_path" => "/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem",
+      "certificate_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem",
+      "private_key_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem"
+    }
+
+    @classify = Classify.new(@classifier_url, auth_info)
+  end
+
+  describe "#update" do
+    it "sends an update message to the classifier" do
+      stub_request(:post, "#{@classifier_url}/v1/update-classes").
+                 with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+                 to_return(:status => 201, :body => "", :headers => {})
+      expect(@classify.update_classes.update).to be_nil
+    end
+  end
+end
