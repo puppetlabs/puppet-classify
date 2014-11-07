5d8cd23e3269dcd84109e5d12416a926f89cfa67
Brian Cain <brian.cain@puppetlabs.com>
2014-11-07 11:51:23 -0800

    Add initial import_hierarchy spec

---
+++ spec/classify/import_hierarchy_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../../lib/classify'
+
+describe ImportHierarchy do
+  before :each do
+    @classifier_url = 'https://puppetmaster.local:4433/classifier-api'
+
+    auth_info = {
+      "ca_certificate_path" => "/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem",
+      "certificate_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem",
+      "private_key_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem"
+    }
+
+    @classify = Classify.new(@classifier_url, auth_info)
+  end
+
+  describe "#import" do
+  end
+end
