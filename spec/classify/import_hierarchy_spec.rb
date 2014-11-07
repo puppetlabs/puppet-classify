7a6a0e977e90a68e4d412cc58b07b622debc428d
Brian Cain <brian.cain@puppetlabs.com>
2014-11-07 13:42:52 -0800

    Add import spec test

--- spec/classify/import_hierarchy_spec.rb
+++ spec/classify/import_hierarchy_spec.rb
@@ -12,8 +12,18 @@ describe ImportHierarchy do
     }
 
     @classify = Classify.new(@classifier_url, auth_info)
+
+    @import_group_hash = [{"environment_trumps"=>false, "parent"=>"00000000-0000-4000-8000-000000000000", "name"=>"default", "rule"=>["and", ["~", "name", ".*"]], "variables"=>{}, "id"=>"00000000-0000-4000-8000-000000000000", "environment"=>"production", "classes"=>{}},{"name"=>"testgroup", "description"=>"A cool group", "environment"=>"production", "parent"=>"00000000-0000-4000-8000-000000000000", "classes"=>{}}]
+
   end
 
   describe "#import" do
+    it "sends a group hash to be imported" do
+      stub_request(:post, "#{@classifier_url}/v1/import-hierarchy").
+         with(:body => @import_group_hash.to_json,
+              :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 204, :body => "", :headers => {})
+      expect(@classify.import_hierarchy.import(@import_group_hash)).to be_nil
+    end
   end
 end
