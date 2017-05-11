6dbcf6710fa48e091909f4a49e64f8fb41f76552
Jesse Reynolds <jesse@va.com.au>
2017-05-11 17:54:26 +1000

    fixed one test

--- spec/puppet_https_spec.rb
+++ spec/puppet_https_spec.rb
@@ -6,12 +6,24 @@ describe PuppetHttps do
     before :each do
       @classifier_url = 'https://puppetmaster.local:4433/classifier-api'
 
+      private_key_path    = "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem"
+      certificate_path    = "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem"
+      ca_certificate_path = "/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem"
+
       auth_info = {
-        "ca_certificate_path" => "/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem",
-        "certificate_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem",
-        "private_key_path"    => "/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem"
+        "private_key_path"    => private_key_path,
+        "certificate_path"    => certificate_path,
+        "ca_certificate_path" => ca_certificate_path,
       }
 
+      expect(File).to receive("exists?").with(certificate_path).and_return(true)
+      expect(File).to receive("exists?").with(private_key_path).and_return(true)
+      expect(File).to receive("exists?").with(ca_certificate_path).and_return(true)
+
+      expect(File).to receive("read").with(certificate_path).and_return('not a cert')
+      expect(File).to receive("read").with(private_key_path).and_return('not a key')
+      expect(OpenSSL::X509::Certificate).to receive("new").with('not a cert').and_return('not a cert object')
+      expect(OpenSSL::PKey::RSA).to receive("new").with('not a key').and_return('not a key object')
       @puppet_https = PuppetHttps.new(auth_info)
     end
 
